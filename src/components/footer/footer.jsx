e76cc8987fa395783767f4646dd310f5c1014be1
Daler Zokirov <zokirov.d@list.ru>
2020-03-07 01:23:42 +0500

    made the  footer component  responsive

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,15 +6,12 @@ import BrandImage from '../../assets/images/asset7.JPG';
 import IphoneButton from '../../assets/images/iPhone.png';
 import GooglePlay from '../../assets/images/GooglePlay.png';
 
-import { FooterMain, H1, H2, H6, Row1, Row2, Wrapper, Image1, Image2 } from './footer.styles';
+import { FooterMain, H1, H2, H6, Row1, Row2, Wrapper, Image1, Image2, H2Light } from './footer.styles';
 
 
 const Footer = () => (
     <FooterMain>
-    <div className='footer'>
         <Container>
-            <Row>
-                <Col>
                     <img src={BrandImage} alt="brand"/>
                     <Row1>
                         <H1>Get Started</H1>
@@ -32,17 +29,13 @@ const Footer = () => (
                         <H1>Legal</H1>
                     </Row2>
                     <H6>© 2019, A Medium Corporation</H6>
-                </Col>
-                <Col>
+                
                     <Wrapper>
-                    <H2>Get the Medium app</H2>
+                    <H2Light>Get the Medium app</H2Light>
                         <Image1><img  src={IphoneButton} alt="iPhone"/></Image1>
                         <Image2><img  src={GooglePlay} alt="iPhone"/></Image2>
                     </Wrapper>
-                </Col>
-            </Row>
         </Container>
-    </div>
 </FooterMain>
 );
 
--- src/components/footer/footer.styles.jsx
+++ src/components/footer/footer.styles.jsx
@@ -3,26 +3,39 @@ import styled from 'styled-components';
 
 export const FooterMain = styled.div`
     width: 100%;
-    height: 265px;
+    height: 100%;
     background-color: #000000;
     margin-top: 80px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
     img {
         width: 35px;
         height: 35px;
         margin-top: 50px;
         margin-left: -16px;
         margin-bottom: 10px;
+        @media screen and ( max-width: 890px ) {
+        margin-left: 20px;
+    }
     }
 `;
 
 export const H1 = styled.h1`
-    font-size: 15px;
+    font-size: 14px;
     color: #FFFFFF;
     margin-right: 33px;
     margin-left: -14px;
-    margin-top: 20px;
+    margin-top: 15px;
     &:hover {
         cursor: pointer;
+    }
+    @media screen and ( max-width: 890px ) {
+        margin-left: 20px;
+        margin-right: 0px;
+        margin-top: 10px;
+        font-size: 14px;
     }
 `;
 
@@ -30,43 +43,107 @@ export const H2 = styled.h2`
     font-size: 15px;
     color: #999999;
     margin-right: 40px;
-    margin-top: 20px;
+    margin-top: 15px;
+    @media screen and ( max-width: 890px ) {
+        margin-left: 20px;
+        margin-right: 0px;
+        margin-top: 10px;
+        font-size: 14px;
+    }
 `;
+
+export const H2Light = styled.h2`
+    font-size: 15px;
+    color: #999999;
+    margin-right: 40px;
+    margin-top: 20px;
+    @media screen and ( max-width: 890px ) {
+        margin-right: 40px;
+        font-size: 14px;
+    }
+    @media screen and ( max-width: 600px ) {
+        margin-left: 20px;
+    }
+`;
+
 
 export const H6 = styled.h6`
     font-size: 13px;
     color: #999999;
     margin-left: -14px;
     margin-top: 15px;
+    @media screen and ( max-width: 890px ) {
+        position: absolute;
+        margin-top: 10.8rem;
+        margin-left: 20px;
+    }
+    @media screen and ( max-width: 600px ) {
+        margin-top: 7rem;
+    }
 `; 
 
 
 export const Row1 = styled.div`
     display: flex;
     flex-direction: row;
+    flex-wrap: wrap;
     justify-content:  flex-start;
-    width: 160%;
+    width: 100%;
 `;
 
 export const Row2 = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: flex-start;
-    width: 160%;
+    flex-wrap: wrap;
+    width: 100%;
 `;
 
 export const Wrapper = styled.div`
     display: flex;
-    flex-flow: column nowrap;
+    flex-flow: column wrap;
     align-items: flex-end;
     justify-content: flex-end;
     margin-right: -50px;
+    margin-top: -13rem;
+    margin-bottom: 50px;
+    @media screen and ( max-width: 890px ) {
+        order: 2;
+        margin-top: 0px;
+        margin-right: 0px;
+        float: left;
+    }
+    @media screen and ( max-width: 880px ) {
+        order: 2;
+        margin-top: 0px;
+        margin-right: 0px;
+        float: left;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start;
+    }
+    @media screen and ( max-width: 600px ) {
+        order: 2;
+        margin-top: 0px;
+        margin-right: 0px;
+        float: left;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: flex-start;
+        margin-bottom: 150px;
+    }
 `;
 
 export const Image1 = styled.div`
     margin-top: -50px;
     margin-right: 30px;
-
+    @media screen and ( max-width: 600px ) {
+        position: absolute;
+        margin-top: 35px;
+        margin-left: -10px;
+}
     img {
         width: 160px;
         height: 60px;
@@ -79,6 +156,12 @@ export const Image1 = styled.div`
 export const Image2 = styled.div`
     margin-top: -110px;
     margin-right: 30px;
+    @media screen and ( max-width: 600px ) {
+        position: absolute;
+        margin-top: 35px;
+        margin-left: 150px;
+}
+
     img {
         width: 155px;
         height: 150px;
