2d20feb7468df5b8559a2e163cc812fdcd43c065
Daler Zokirov <zokirov.d@list.ru>
2020-04-02 17:02:14 +0500

    some updates

--- src/containers/Nav/index.js
+++ src/containers/Nav/index.js
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { withRouter, Route, useParams } from "react-router-dom";
 
-import { getNav } from '../../redux/modules/navbar/navAction';
-import { NavStyled, NavLinkStyled, NavItem } from './style';
-import links from '../../mock-data/categories';
-
+import { getNav } from "../../redux/modules/navbar/navAction";
+import { NavStyled, NavLinkStyled, NavItem } from "./style";
+import links from "../../mock-data/categories";
 
 const NavMain = (props) => {
   useEffect(() => {
@@ -14,33 +13,29 @@ const NavMain = (props) => {
 
   return (
     <NavStyled activeKey="/home">
-      {
-        links
-          .map((link) => (
-            <NavItem key={link.id} link={link}>
-              <NavLinkStyled
-                key={link.id}
-                link={link}
-                to={link.name}
-              >
-                {link.name}
-              </NavLinkStyled>
-            </NavItem>
-          ))
-      }
+      {links.map(link => (
+        <NavItem key={link.id} link={link}>
+          <NavLinkStyled
+            key={link.id}
+            link={link}
+            to={`/categories/${link.name}`}
+          >
+            {link.name}
+          </NavLinkStyled>
+        </NavItem>
+      ))}
     </NavStyled>
   );
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   token: state.authReducer.token,
   loading: state.authReducer.loading,
   error: state.authReducer.error
 });
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   getNav: () => dispatch(getNav())
 });
-
 
 export default connect(null, mapDispatchToProps)(withRouter(NavMain));
